9acfb0ff93d5ee4420edc2c2b283b0488af024dc
xudong7930 <xudong7930@gmail.com>
2015-09-21 10:31:06 +0800

    增加商城常用函数
    增加商城常用函数,生成唯一订单号

---
+++ shop/common.php
@@ -0,0 +1,18 @@
+<?php
+
+//唯一订单号
+function ordersn2(){
+    $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+    $orderSn = $yCode[intval(date('Y')) - 2015].date("YmdHis",time()).sprintf('%03d%02d', rand(100, 999),rand(0,99));
+    return $orderSn;
+}
+
+
+//生产唯一订单号
+function ordersn3(){
+	$yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+	$orderSn = $yCode[intval(date('Y')) - 2015] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%04d%02d', rand(1000, 9999),rand(0,99));
+	return $orderSn;
+}
+
+
